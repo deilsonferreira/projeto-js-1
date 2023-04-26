3432b522d443a38b8210c52efb030abd0c5b3c58
deilsonferreira <106764067+deilsonferreira@users.noreply.github.com>
2023-04-26 17:10:04 -0300

    Add files via upload

---
+++ Exercicio-1-JS/script.js
@@ -0,0 +1,88 @@
+// SOMA
+
+// alert("Vamos calcular??")
+
+// let num1 = prompt ("Insira o primeiro número")
+// let num2 = prompt("Insira o segundo número")
+
+// num1 = parseFloat(num1)
+// num2 = parseFloat(num2)
+
+// let resultado = num1+num2
+
+// alert("A soma é " + resultado)
+
+
+// SUBTRAÇÃO
+
+// alert("Vamos calcular??")
+
+// let num1 = prompt ("Insira o primeiro número")
+// let num2 = prompt("Insira o segundo número")
+
+// num1 = parseFloat(num1)
+// num2 = parseFloat(num2)
+
+// let resultado = num1-num2
+
+// alert("A subtração é " + resultado)
+
+//MULTIPLICAÇÃO
+
+// alert("Vamos calcular??")
+
+// let num1 = prompt ("Insira o primeiro número")
+// let num2 = prompt("Insira o segundo número")
+
+// num1 = parseFloat(num1)
+// num2 = parseFloat(num2)
+
+// let resultado = num1*num2
+
+// alert("A multiplicação é " + resultado)
+
+
+//DIVISÃO  
+
+// alert("Vamos calcular??")
+
+// let num1 = prompt ("Insira o primeiro número")
+// let num2 = prompt("Insira o segundo número")
+
+// num1 = parseFloat(num1)
+// num2 = parseFloat(num2)
+
+// let resultado = num1/num2
+
+// alert("A divisão é " + resultado)
+
+// RESTO
+
+// alert("Vamos calcular??")
+
+// let num1 = prompt ("Insira o primeiro número")
+// let num2 = prompt("Insira o segundo número")
+
+// num1 = parseFloat(num1)
+// num2 = parseFloat(num2)
+
+// let resultado = num1/num2 / 2
+
+// alert("A divisão é " + resultado)
+
+
+
+//SOMA
+
+alert("Vamos calcular??")
+
+let num1 = prompt ("Insira o primeiro número")
+let num2 = prompt("Insira o segundo número")
+
+num1 = parseFloat(num1)
+num2 = parseFloat(num2)
+
+let resultado = num1+num2
+
+alert("A soma é " + resultado % 2 === 0) 
+
